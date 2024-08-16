0adf9a731c0f05fddeb838f96b38b2f981456181
nadertoti <88191814+nadertoti@users.noreply.github.com>
2024-08-16 19:24:06 +0400

    Delete chatbot-project/server directory

--- chatbot-project/server/routes/chatbot.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const knowledgeBaseController = require('../controllers/knowledgeBaseController');
-
-router.post('/ask', knowledgeBaseController.handleQuery);  // Direct call
-
-module.exports = router;
--- chatbot-project/server/routes/knowledgeBase.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const knowledgeBaseController = require('../controllers/knowledgeBaseController');
-
-// Route to handle user queries
-router.post('/query', knowledgeBaseController.handleQuery);
-
-module.exports = router;
